d98fe95be9c490199be73eec2dd4664c73552739
Duckonaut <duckonaut@gmail.com>
2022-05-03 16:47:00 +0200

    change over to clap from structopt

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
-use structopt::StructOpt;
+use clap::Parser;
 
 mod generator;
 mod data;
 
-#[derive(StructOpt, Debug)]
-#[structopt(name = "blockblog", about = "YAML and Markdown based static HTML generator")]
+#[derive(Parser, Debug, Clone)]
+#[clap(name = "blockblog", about = "YAML and Markdown based static HTML generator")]
 enum Args {
-    #[structopt(name = "generate", about = "Generate all static HTML pages")]
+    #[clap(name = "generate", about = "Generate all static HTML pages")]
     Generate {
-        #[structopt(short = "i", long = "input", parse(from_os_str), default_value = ".", help = "Input directory")]
+        #[clap(short = 'i', long = "input", parse(from_os_str), default_value = ".", help = "Input directory")]
         input: std::path::PathBuf,
-        #[structopt(short = "o", long = "output", parse(from_os_str), default_value = "./output", help = "Output directory")]
+        #[clap(short = 'o', long = "output", parse(from_os_str), default_value = "./output", help = "Output directory")]
         output: std::path::PathBuf,
-        #[structopt(short = "s", long = "safe", help = "Do not remove output directory files already present")]
+        #[clap(short = 's', long = "safe", help = "Do not remove output directory files already present")]
         safe: bool,
     }
 }
 
 fn main() {
-    let args = Args::from_args();
+    let args = Args::parse();
 
     match args {
         Args::Generate { input, output, safe } => generator::generate(input, output, safe),
